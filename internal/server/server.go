cfb02bb4e4806061188092240b373ed2b03b4852
kamemia <kamemia18@yahoo.com>
2026-06-29 09:12:00 +0300

    - Created initial server setup in internal/server with router configuration. - Added empty packages for stream and upload functionality.

---
+++ internal/server/server.go
@@ -0,0 +1,15 @@
+package server
+
+import (
+	"github.com/go-chi/chi/v5"
+)
+
+func setupRouter() *chi.Mux {
+	r := chi.NewRouter()
+
+	return r
+}
+
+// func setupRoutes(server *Server, r *chi.Mux) {
+
+// }
